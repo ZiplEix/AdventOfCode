9a044fa0a2b40c109d2e6778eba09c3ea99bb578
Baptiste Leroyer <86067803+ZiplEix@users.noreply.github.com>
2022-12-01 14:27:50 +0000

    day 1 part 1

---
+++ day1/part1.go
@@ -0,0 +1,33 @@
+// a hello word program
+
+package main
+
+import (
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	input, err := os.ReadFile("./input")
+	if err != nil {
+		panic(err)
+	}
+	inputString := string(input)
+	result := 0
+
+	elfs := strings.Split(inputString, "\n\n")
+	for _, elf := range elfs {
+		calories := strings.Split(elf, "\n")
+		elfFood := 0
+		for _, calorie := range calories {
+			Kca, _ := strconv.Atoi(calorie)
+			elfFood += Kca
+		}
+		if elfFood > result {
+			result = elfFood
+		}
+	}
+
+	println(result)
+}
